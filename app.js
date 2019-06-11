cfb01605dbd027e1e69a8d80eaccb8679c21dac2
rabindranathfv <rabindranathucv@gmail.com>
2019-06-11 18:54:14 -0400

    using axios configure specifig headers and get request by city name

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 const argv = require('./config/yargs').argv;
+const axios = require('axios');
 
-console.log(argv);
-git
+// encodeUri add special character for safety use with nodejs
+const encodeUrl = encodeURI(argv.address);
+
+console.log(encodeUrl);
+
+const instance = axios.create({
+    baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodeUrl}`,
+    headers: { 'X-RapidAPI-Key': '09361c6843msh4cdfa7663e8191bp1cc01bjsnf225f6d88e10' }
+});
+
+instance.get()
+    .then(resp => {
+        console.log(resp.data.Results[0]);
+    })
+    .catch(err => {
+        console.log('**** Error ***', err);
+    });
